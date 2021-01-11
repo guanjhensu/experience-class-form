fee1173fe8c0b87eb6ffeba8b712df804b7dec24
guanjhensu <guanjhensu@gmail.com>
2021-01-11 16:42:44 +0800

    Adding Custom Environment Variables starting with REACT_APP_

--- src/api/UnsplashAPI.js
+++ src/api/UnsplashAPI.js
@@ -1,7 +1,7 @@
 import { createApi } from 'unsplash-js';
 
 const UnsplashAPI = createApi({
-  accessKey: process.env.UNSPLASH_ACCESS_KEY
+  accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY
 });
 
 export default UnsplashAPI;
