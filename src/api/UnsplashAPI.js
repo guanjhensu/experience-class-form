69a57a392c3399deb3184d179504975f449e8af3
guanjhensu <guanjhensu@gmail.com>
2021-01-11 16:26:07 +0800

    deploy to heroku

--- src/api/UnsplashAPI.js
+++ src/api/UnsplashAPI.js
@@ -2,7 +2,7 @@ import { createApi } from 'unsplash-js';
 import config from '../config';
 
 const UnsplashAPI = createApi({
-  accessKey: config.UNSPLASH_ACCESS_KEY
+  accessKey: process.env.UNSPLASH_ACCESS_KEY
 });
 
 export default UnsplashAPI;
